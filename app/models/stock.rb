be21d84f1f8bb652fff40d6aa0db3c023ddc9add
Inioluwa Akinleye <inioluwaakinleye@gmail.com>
2018-04-15 23:46:47 +0100

    lookup stock details

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,3 +1,13 @@
 class Stock < ApplicationRecord
 
+  def self.new_from_lookup(ticker_symbol)
+    looked_up_stock = StockQuote::Stock.quote(ticker_symbol)
+    price = strip_commas(looked_up_stock.l)
+    new(looked_up_stock.name, ticker: looked_up_stock.symbol, last_price: price)
+  end
+
+  def self.strip_commas(number)
+    number.gsub(',', '')
+  end
+
 end
