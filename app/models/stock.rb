c58ec7704acee31699d864207f1c0914a9e998b8
Inioluwa Akinleye <inioluwaakinleye@gmail.com>
2018-04-15 23:51:11 +0100

    refactor

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -3,10 +3,10 @@ class Stock < ApplicationRecord
   def self.new_from_lookup(ticker_symbol)
     looked_up_stock = StockQuote::Stock.quote(ticker_symbol)
     price = strip_commas(looked_up_stock.l)
-    new(looked_up_stock.name, ticker: looked_up_stock.symbol, last_price: price)
+    new(name: looked_up_stock.name, ticker: looked_up_stock.symbol, last_price: price)
   end
 
-  def self.strip_commas(number)
+  def strip_commas(number)
     number.gsub(',', '')
   end
 
