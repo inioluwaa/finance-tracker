cca7aaea990c432e1f226fa3f88bfb1b764300ba
Inioluwa Akinleye <inioluwaakinleye@gmail.com>
2018-04-17 05:29:23 +0100

    work with invalid and empty search symbol

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -1,8 +1,19 @@
 class StocksController < ApplicationController
 
   def search
-    @stock = Stock.new_from_lookup(params[:stock])
-    render 'users/my_portfolio'
+    if params[:stock].present?
+      @stock = Stock.new_from_lookup(params[:stock])
+      if @stock
+        render 'users/my_portfolio'
+      else
+        flash[:danger] = 'You have entered an incorrect symbol'
+        redirect_to my_portfolio_path
+      end
+    else
+      flash[:danger] = 'You have entered an empty search string'
+      redirect_to my_portfolio_path
+    end
+
   end
 
 end
--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,9 +1,13 @@
 class Stock < ApplicationRecord
 
   def self.new_from_lookup(ticker_symbol)
-    looked_up_stock = StockQuote::Stock.quote(ticker_symbol)
-    price = strip_commas(looked_up_stock.l)
-    new(name: looked_up_stock.name, ticker: looked_up_stock.symbol, last_price: price)
+    begin
+      looked_up_stock = StockQuote::Stock.quote(ticker_symbol)
+      price = strip_commas(looked_up_stock.l)
+      new(name: looked_up_stock.name, ticker: looked_up_stock.symbol, last_price: price)
+    rescue Exception => e
+      return nil
+    end
   end
 
   def strip_commas(number)
