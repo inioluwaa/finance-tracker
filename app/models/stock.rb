d7b405ac59791b9c652cae931a13e1aba18691bf
Inioluwa Akinleye <inioluwaakinleye@gmail.com>
2018-04-19 22:58:31 +0100

    fix code

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,9 +1,13 @@
 class Stock < ApplicationRecord
 
   def self.new_from_lookup(ticker_symbol)
-    looked_up_stock = StockQuote::Stock.quote(ticker_symbol)
-    new(name: looked_up_stock.company_name, ticker: looked_up_stock.symbol,
-        last_price: looked_up_stock.latest_price)
+    begin
+      looked_up_stock = StockQuote::Stock.quote(ticker_symbol)
+      new(name: looked_up_stock.company_name, ticker: looked_up_stock.symbol,
+          last_price: looked_up_stock.latest_price)
+    rescue Exception => e
+      return nil unless looked_up_stock.company_name
+    end
   end
 
 end
