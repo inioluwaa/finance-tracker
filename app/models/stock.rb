2afbf0f0bf3799b4160b58164a2d6f63b69fe9c0
Inioluwa Akinleye <inioluwaakinleye@gmail.com>
2018-04-22 17:05:27 +0100

    stocks restrictions

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -2,6 +2,10 @@ class Stock < ApplicationRecord
   has_many :user_stocks
   has_many :users, through: :user_stocks
 
+  def self.find_by_ticker(ticker_symbol)
+    where(ticker: ticker_symbol).first
+  end
+
   def self.new_from_lookup(ticker_symbol)
     begin
       looked_up_stock = StockQuote::Stock.quote(ticker_symbol)
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,18 @@ class User < ApplicationRecord
 
   has_many :user_stocks
   has_many :stocks, through: :user_stocks
+
+  def stock_already_added?(ticker_symbol)
+    stock = Stock.find_by_ticker(ticker_symbol)
+    return false unless stock
+    user_stocks.where(stock_id: stock.id).exists?
+  end
+
+  def under_stock_limit?
+    (user_stocks.count < 10)
+  end
+
+  def can_add_stock?(ticker_symbol)
+    under_stock_limit? && !stock_already_added?(ticker_symbol)
+  end
 end
