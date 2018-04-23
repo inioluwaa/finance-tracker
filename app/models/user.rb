1e259d850ad0abba9d0a12fa7338a02d73453324
Inioluwa Akinleye <inioluwaakinleye@gmail.com>
2018-04-23 06:07:00 +0100

    add user detail

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,11 @@ class User < ApplicationRecord
   has_many :user_stocks
   has_many :stocks, through: :user_stocks
 
+  def full_name
+    return "#{first_name}" "#{last_name}".strip if (first_name || last_name)
+    'Anonymous'
+  end
+
   def stock_already_added?(ticker_symbol)
     stock = Stock.find_by_ticker(ticker_symbol)
     return false unless stock
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180422011636) do
+ActiveRecord::Schema.define(version: 20180423043009) do
 
   create_table "stocks", force: :cascade do |t|
     t.string "ticker"
@@ -42,6 +42,8 @@ ActiveRecord::Schema.define(version: 20180422011636) do
     t.string "last_sign_in_ip"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "first_name"
+    t.string "last_name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
