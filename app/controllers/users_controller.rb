8bd871b2c3e328adc08d55b5284af04b8602f942
Inioluwa Akinleye <inioluwaakinleye@gmail.com>
2018-04-23 05:24:34 +0100

    add stock listing and remove function

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   def my_portfolio
-    
+    @user = current_user
+    @user_stocks = current_user.stocks
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,5 @@ Rails.application.routes.draw do
   get 'my_portfolio', to: 'users#my_portfolio'
   get 'search_stocks', to: 'stocks#search'
 
-  resources :user_stocks, only: [:create]
+  resources :user_stocks, only: %i[create destroy]
 end
