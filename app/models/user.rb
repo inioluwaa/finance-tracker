0386cfab502ec10df445c25d15bde73c25e41d34
Inioluwa Akinleye <inioluwaakinleye@gmail.com>
2018-04-24 00:06:42 +0100

    update views and navigation

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,7 @@ class User < ApplicationRecord
   has_many :stocks, through: :user_stocks
 
   def full_name
-    return "#{first_name}" "#{last_name}".strip if (first_name || last_name)
+    return "#{first_name} #{last_name}".strip if (first_name || last_name)
     'Anonymous'
   end
 
