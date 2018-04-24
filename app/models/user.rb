ac93d5ae25ba93ad2e78b45d71b4649188898241
Ini Akinleye <inioluwaakinleye@gmail.com>
2018-04-24 07:30:29 +0100

    Add friendship model

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,8 @@ class User < ApplicationRecord
 
   has_many :user_stocks
   has_many :stocks, through: :user_stocks
-
+  has_many :friendships
+  has_many :friends, through: :friendships
   def full_name
     return "#{first_name} #{last_name}".strip if (first_name || last_name)
     'Anonymous'
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   root 'welcome#index'
   get 'my_portfolio', to: 'users#my_portfolio'
   get 'search_stocks', to: 'stocks#search'
+  get 'my_friends', to: 'users#my_friends'
 
   resources :user_stocks, only: %i[create destroy]
 end
