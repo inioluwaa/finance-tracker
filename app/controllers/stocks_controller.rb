7e9f63b6f8e336a9f1a84bb170d7dfa569536b23
Ini Akinleye <inioluwaakinleye@gmail.com>
2018-04-25 10:32:36 +0100

    search action and errors

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -2,7 +2,7 @@ class StocksController < ApplicationController
 
   def search
     if params[:stock].blank?
-      flash.now[:danger] = 'You have entered an empty search string'#
+      flash.now[:danger] = 'You have entered an empty search string'
     else
       @stock = Stock.new_from_lookup(params[:stock])
       flash.now[:danger] = 'You have entered an incorrect symbol' unless @stock
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,7 +9,14 @@ class UsersController < ApplicationController
   end
 
   def search
-    @users = User.search(params[:search_param])
-    render json: @users
+    if params[:search_param].blank?
+      flash.now[:danger] = 'You have entered an empty search string'
+    else
+      @users = User.search(params[:search_param])
+      flash.now[:danger] = 'No users match this search criteria' if @users.blank?
+    end
+    respond_to do |format|
+      format.js { render partial: 'friends/result' }
+    end
   end
 end
