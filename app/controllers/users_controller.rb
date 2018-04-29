8fc98845fe8e8bee084db5c8b57df074b16ebdd0
Ini Akinleye <inioluwaakinleye@gmail.com>
2018-04-29 21:03:24 +0100

    add friend show action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -32,4 +32,9 @@ class UsersController < ApplicationController
     end
     redirect_to my_friends_path
   end
+
+  def show
+    @user = User.find(params[:id])
+    @user_stocks = @user.stocks
+  end
 end
