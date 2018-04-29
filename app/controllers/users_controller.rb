a471fc3f6560fcceb2cf7438ca74f46dc261a242
Ini Akinleye <inioluwaakinleye@gmail.com>
2018-04-29 20:14:57 +0100

    add friends

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -22,6 +22,14 @@ class UsersController < ApplicationController
   end
 
   def add_friend
+    @friend = User.find(params[:friend])
+    current_user.friendships.build(friend_id: @friend.id)
 
+    if current_user.save
+      flash[:success] = 'Friend was successfully added'
+    else
+      flash[:danger] = 'There was something wrong with the friend request'
+    end
+    redirect_to my_friends_path
   end
 end
